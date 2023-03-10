4cf54f7069348993c8d632f033960f26797e6692
ivonne1825 <ivon251802@gmail.com>
2023-03-09 20:54:29 -0500

    cambios corregidos

--- cypress/e2e/m/T/Elements/textbox/textbox.cy.js
+++ cypress/e2e/m/T/Elements/textbox/textbox.cy.js
@@ -1,21 +1,26 @@
 import TextboxPO from "./textboxPO";
 
 describe('test01', () => {
+    
+    beforeEach( function() {
+        cy.visit("https://demoqa.com/");
+        cy.url().should('include', 'https://demoqa.com/');
+        cy.fixture('textbox.json').as('Textboxdata');
+    });
     it('test001', () => {
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false
         })
-        cy.visit("https://demoqa.com/") // Esto debe ir en el método beforeEach, Andrew.
 
         const text = new TextboxPO();
 
-        cy.wait(5000) // Eliminar este tiempo, Andrew. 
+
         text.buttonElemen().click();
         text.buttontextb().click();
-        text.completeName().type("ivonne").should("have.value", "ivonne"); // Agrega el usuario desde la carpeta de fixture
-        text.email().type("ivonne@gmail.com").should("have.value", "ivonne@gmail.com");
-        text.curAdress().type("adress").should("have.value", "adress");
-        text.permAdress().type("perAdress").should("have.value", "perAdress");
+        text.completeName().type(this.Textboxdata.name).should("have.value", "ivonne"); // Agrega el usuario desde la carpeta de fixture
+        text.email().type(this.Textboxdata.email).should("have.value", "ivonne@gmail.com");
+        text.curAdress().type(this.Textboxdata.currentAddress).should("have.value", "adress");
+        text.permAdress().type(this.Textboxdata.permanentAddress).should("have.value", "perAdress");
         text.buttonSub().click()
 
         cy.get("#name").should('be.visible'),
