43506d6b999e9684b4554c066a02e8bc1287ccef
andrew.ruz@tangelolatam.com <andrew.ruz@tangelolatam.com>
2023-03-09 20:27:16 -0500

    Fix

--- cypress/e2e/visa_test/Elements/Practice DEMOQA/Textbox/textbox.cy.js
+++ cypress/e2e/visa_test/Elements/Practice DEMOQA/Textbox/textbox.cy.js
@@ -1,18 +1,15 @@
 import Textbox from "./textbox";
-
-
 describe('Test Textbox', () => { 
     beforeEach(()=> {
         cy.visit(Cypress.env("baseUrlDemoQA"));
         cy.url().should('include', Cypress.env("baseUrlDemoQA"));
 
-        cy.get('div[class="category-cards"] div:nth-child(1) div:nth-child(1) div:nth-child(3)').click()
+        cy.get('div[class="category-cards"] div:nth-child(1) div:nth-child(1) div:nth-child(3)').click() // what's???? mala practica escoger un Xpath
         cy.get('.main-header').should('have.text', 'Elements')
 
-        cy.get( "div[class='element-list collapse show'] li[id='item-0'] span[class='text']")
+        cy.get( "div[class='element-list collapse show'] li[id='item-0'] span[class='text']") // Mismo, el path no es buena practica. 
         .click()
     })
-
 
     it('Textbox test', () => {
 
@@ -20,23 +17,12 @@ describe('Test Textbox', () => {
             return false
         })
 
-     const ln = new Textbox() ;
-           
-
-            ln.inputFullName('Luis Penagos')
-       
-            ln.inputEmail('test@test.com')
-            
-            ln.inputCurrentAddress('Kra 27 # 17 B 05')
-            
-            ln.inputPermanentAddress('Cll 29 c Sur # 41 - 34')
-            
+     const ln = new Textbox() ; // Debes cambiar el nombre de la variable, para tener buenas practicas en la semantica. 
+            ln.inputFullName('Luis Penagos')      // Agrega estos valores desde el fixture
+            ln.inputEmail('test@test.com')          
+            ln.inputCurrentAddress('Kra 27 # 17 B 05')          
+            ln.inputPermanentAddress('Cll 29 c Sur # 41 - 34')           
             ln.submitBtn()
-
             ln.assertion()
-
-
-
     })
-
 })
