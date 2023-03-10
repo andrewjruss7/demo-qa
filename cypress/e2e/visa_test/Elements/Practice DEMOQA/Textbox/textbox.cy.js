210106c1f41a9a9f63acdd5fb16dd28f7551b914
andrew.ruz@tangelolatam.com <andrew.ruz@tangelolatam.com>
2023-03-09 21:27:36 -0500

    solucion, cracks

--- cypress/e2e/visa_test/Elements/Practice DEMOQA/Textbox/textbox.cy.js
+++ cypress/e2e/visa_test/Elements/Practice DEMOQA/Textbox/textbox.cy.js
@@ -1,4 +1,5 @@
 import Textbox from "./textbox";
+
 describe('Test Textbox', () => { 
     beforeEach(()=> {
       
@@ -9,27 +10,24 @@ describe('Test Textbox', () => {
         cy.get('.main-header').should('have.text', 'Elements')
 
         cy.get('#item-0').click()
-
-
-
-
-    })
-
-    
+    }) 
 
     it('Textbox test', () => {
 
+        const credentials = require('../../../../../fixtures/textBoxData.json')
+        const inputText = new Textbox()  
+
+        
+     
         Cypress.on('uncaught:exception', (err, runnable) => {
              return false
          })
      
-         cy.fixture('textBoxData.json').as("userData")
-     
-         const inputText = new Textbox() ; // Cambia el nombre de la variable para tener buenas prácticas semánticas
-         inputText.inputFullName(this.userData.name)
-         inputText.inputEmail(this.userData.email)
-         inputText.inputCurrentAddress(this.userData.address)
-         inputText.inputPermanentAddress(this.userData.padress)
+          ; // Cambia el nombre de la variable para tener buenas prácticas semánticas
+        inputText.inputFullName(credentials.name)
+        inputText.inputEmail(credentials.email)
+        inputText.inputCurrentAddress(credentials.address)
+        // inputText.inputPermanentAddress(this.credentials.padress)
          inputText.submitBtn()
      
          cy.get('#name').should('be.visible')
