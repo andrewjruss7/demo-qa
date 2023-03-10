584f14f384dcec7fa5e14a843e14e1c98cb93b63
andrew.ruz@tangelolatam.com <andrew.ruz@tangelolatam.com>
2023-03-09 20:55:42 -0500

    ok

--- cypress/e2e/Elements/Textbox.cy.js
+++ cypress/e2e/Elements/Textbox.cy.js
@@ -1,23 +1,21 @@
-    
-describe ('Text Box', function(){
-    it('text box', function() { 
-        cy.visit(("https://demoqa.com/"));
 
+describe('Text Box', function () {
+    it('text box', function () {
+        cy.visit(("https://demoqa.com/")); // Agregar el visit desde el metodo beforeEach
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false
         })
-        
-    
-cy.get("h5").contains("Elements").click()
+        cy.get("h5").contains("Elements").click()
 
-cy.get("div[class='element-list collapse show'] li[id='item-0'] span[class='text']").click(),
+        cy.get("div[class='element-list collapse show'] li[id='item-0'] span[class='text']").click(), // Quitar coma, esta de mas y el xpath es mala practica.
 
-cy.get('#userName').type('isela maria');
-cy.get('#userEmail').type('isela@maria.com');
-cy.get('#currentAddress').type('Paris');
-cy.get('#permanentAddress').type('Francia');
+        cy.get('#userName').type('isela maria'); // Agregar los datos desde fixture. 
+        cy.get('#userEmail').type('isela@maria.com');
+        cy.get('#currentAddress').type('Paris');
+        cy.get('#permanentAddress').type('Francia');
 
-cy.get('#submit').click();
-
+        cy.get('#submit').click(); // Agregar los selectores desde el archivo PO
+    })
 })
-})
+
+// y para finalizar agrega este codigo como un comando que pueda ser reutilizable, tu puedess Iseee!!! 
--- cypress/e2e/Elements/TextboxPO.js
+++ cypress/e2e/Elements/TextboxPO.js
@@ -1,40 +1,39 @@
 class Texbox {
     elements = {
         buttonElements: () => cy.get("h5"),
-        buttonTextBox: ()=> cy.get("div[class='element-list collapse show'] li[id='item-0'] span[class='text']"),
-        fullName:()=> cy.get('#userName'),
-        emailWriting: ()=> cy.get('#userEmail'),
-        CurrentAddress : ()=> cy.get('#currentAddress'),
-        PermanentAddress:()=> cy.get('#permanentAddress'),
-        buttonSubmit:()=> cy.get('#submit'),
+        buttonTextBox: () => cy.get("div[class='element-list collapse show'] li[id='item-0'] span[class='text']"),
+        fullName: () => cy.get('#userName'),
+        emailWriting: () => cy.get('#userEmail'),
+        CurrentAddress: () => cy.get('#currentAddress'),
+        PermanentAddress: () => cy.get('#permanentAddress'),
+        buttonSubmit: () => cy.get('#submit'),
     }
 
-        buttonElements(){
-            return this.elements.buttonElements()
-        }
-        buttonTextBox(){
-            return this.elements.buttonTextBox()
-        }
-
-        inputfullName(){
-            return this.elements.fullName()
-
-        }
-
-        inputemailWriting(){
-            return this.elements.emailWriting
-        
+    buttonElements() {
+        return this.elements.buttonElements()
     }
-        inputCurrentAddress(){
-            return this.elements.CurrentAddress
-        }
-        inputPermanentAddress(){
-            return this.elements.PermanentAddress
-
-        }
-        inputbuttonSubmit(){
-            return this.elements.buttonSubmit
-
-        }
+    buttonTextBox() {
+        return this.elements.buttonTextBox()
     }
-    
+
+    inputfullName() {
+        return this.elements.fullName()
+
+    }
+
+    inputemailWriting() {
+        return this.elements.emailWriting
+
+    }
+    inputCurrentAddress() {
+        return this.elements.CurrentAddress
+    }
+    inputPermanentAddress() {
+        return this.elements.PermanentAddress
+
+    }
+    inputbuttonSubmit() {
+        return this.elements.buttonSubmit
+
+    }
+}
