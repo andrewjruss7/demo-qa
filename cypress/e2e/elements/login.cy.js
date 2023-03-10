8e68fcea342e0f1b5135cf9de9394f6b1b35ab12
Crous005 <ing.juancguevara@gmail.com>
2023-03-09 19:55:20 -0500

    save

--- cypress/e2e/elements/login.cy.js
+++ cypress/e2e/elements/login.cy.js
@@ -1,27 +1,24 @@
 import LoginPO from "./loginPO"
 
 describe("Prueba de texto", ()=>{
-    Cypress.on('uncaught:exception', (err, runnable) => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
         return false
     })
-    beforeEach(()=>{
+        beforeEach(()=>{
         cy.visit(Cypress.env("baseUrl"))
         cy.url().should('include', Cypress.env("baseUrl"))
         
         cy.get('div[class=category-cards] div:nth-child(1) div:nth-child(1) div:nth-child(3)').click({force:true})
     })
-    it("ingresar datos a text Box",()=>{
+        it("ingresar datos a text Box",()=>{
         const login = new LoginPO
-        
-              
+                      
         login.buttonText()
-        login.fillName()
+        login.fillName("Juan Guevara")
         login.fillEmail("cdmdn@hotmail.com")
         login.fillCurrentaddress("Carrera 27 #17 b05")
         login.fillpermanentAddress("Carrera 27 #17 b05")
         login.submit()
         cy.get("#name").should("be.visible")
-       
+           })
     })
-    
-})
